56e4d598abd0cdfe73d79e53f927f9023300b0ef
cloudmew <claudiawisniewski@freenet.de>
2021-08-11 15:27:42 +0200

    test

---
+++ precache-manifest.f0c9457d152b9ee65e0c0ed70ed1c677.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21e4e4e43a925fad08bf13bff222b15",
+    "url": "aromafusion.webmanifest"
+  },
+  {
+    "revision": "a3c44c40ef14d46dd3bd",
+    "url": "css/app.e7e639ea.css"
+  },
+  {
+    "revision": "7a5c5e654aecdbbc5f52",
+    "url": "css/chunk-vendors.f92b926d.css"
+  },
+  {
+    "revision": "9216f032c6b3de31e4139037d6b44040",
+    "url": "index.html"
+  },
+  {
+    "revision": "a3c44c40ef14d46dd3bd",
+    "url": "js/app.4195bf6a.js"
+  },
+  {
+    "revision": "7a5c5e654aecdbbc5f52",
+    "url": "js/chunk-vendors.3b10375f.js"
+  },
+  {
+    "revision": "7e0a65d6772f58e91a248f86935d58c1",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
